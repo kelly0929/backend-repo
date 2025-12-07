409bdcab5190c0ab1dcd6a6720e68897e5fea896
kelly <michitoshi.mohri@gmail.com>
2025-12-07 15:26:27 +0900

    Fix SSL connection for Azure MySQL

--- db_control/connect_MySQL.py
+++ db_control/connect_MySQL.py
@@ -1,5 +1,4 @@
 from sqlalchemy import create_engine
-
 import os
 from dotenv import load_dotenv
 
@@ -12,18 +11,25 @@ DB_PASSWORD = os.getenv('DB_PASSWORD')
 DB_HOST = os.getenv('DB_HOST')
 DB_PORT = os.getenv('DB_PORT')
 DB_NAME = os.getenv('DB_NAME')
+DB_SSL_CA = os.getenv('DB_SSL_CA')  # ← 環境変数名を修正
 
 # MySQLのURL構築
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-SSL_CA_PATH = os.getenv('SSL_CA_PATH')
+# SSL設定
+connect_args = {}
+if DB_SSL_CA:
+    # SSL証明書パスが指定されている場合
+    connect_args["ssl"] = {"ca": DB_SSL_CA}
+else:
+    # SSL証明書パスがない場合でも、SSL接続を有効化
+    connect_args["ssl"] = True
+
 # エンジンの作成
 engine = create_engine(
     DATABASE_URL,
     echo=True,
     pool_pre_ping=True,
     pool_recycle=3600,
-    connect_args={
-        "ssl_ca": SSL_CA_PATH
-    }
+    connect_args=connect_args
 )
